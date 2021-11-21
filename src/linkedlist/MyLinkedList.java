1d89dd3bfff9652feb48a52a1cbc4538a854514b
harsargsyan <harsargsyan@gmail.com>
2021-11-22 01:09:23 +0400

    added linked list implementation

--- src/linkedlist/MyLinkedList.java
+++ src/linkedlist/MyLinkedList.java
@@ -1,5 +1,130 @@
 package linkedlist;
 
 public class MyLinkedList {
-    Node head;
+    public Node head;
+
+//    print the linked list nodes
+    public void printList() {
+        Node currNode = this.head;
+
+        while (currNode != null) {
+            System.out.print(currNode.data+" -> ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+//    insert a node at the last position
+    public void insertLast(int data) {
+        Node newNode = new Node(data);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node last = this.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+//    insert a node at the first position
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (this.head != null) {
+            newNode.next = this.head;
+        }
+        this.head = newNode;
+    }
+
+//    insert a node at Nth position
+    public void insertNth(int data, int position) {
+        Node newNode = new Node(data);
+
+//        if head is null and position is zero then exit
+        if (this.head == null) {
+            if (position != 0) {
+                return;
+            } else {
+                this.head = newNode;
+            }
+        }
+
+//        head is not null
+        if (position == 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+            return;
+        }
+
+        Node currentNode = this.head;
+        Node previousNode = null;
+        int i = 0;
+
+        while (i < position) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+
+            if (currentNode == null) {
+                break;
+            }
+
+            i++;
+        }
+
+//        if statement is to avoid null pointer exception
+        if (previousNode != null) {
+            previousNode.next = newNode;
+        }
+
+        newNode.next = currentNode;
+    }
+
+    public int getNth(int index) {
+        Node currentNode = this.head;
+        int count = 0;
+
+        while(currentNode != null) {
+            if (count == index) {
+                return currentNode.data;
+            }
+            count++;
+            currentNode = currentNode.next;
+            }
+
+        System.out.print("index out of range։ ");
+        return 0;
+        }
+
+//    delete a node by position
+    public void deleteByPosition(int position) {
+        Node currentNode = this.head,
+                previousNode = null; // previous node is needed to change currNode.next
+
+//        If index is 0, then head node itself is to be deleted
+        if (position == 0 && currentNode != null) {
+            this.head = currentNode.next;
+        }
+
+//        If the index is greater than 0 but less than the size of LinkedList
+        int counter = 0;
+
+        while (currentNode != null) {
+            if (counter == position) {
+                previousNode.next = currentNode.next;
+                System.out.println(position+" position element deleted");
+                break;
+            } else {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+                counter++;
+            }
+        }
+
+        if (currentNode == null) {
+            System.out.println(position+" position element not found");
+        }
+    }
 }
--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -1,44 +1,10 @@
 package linkedlist;
 
-public class Node {
+class Node {
     int data;
     Node next;
 
-    public Node(int d) {
+    Node(int d) {
         data = d;
-    }
-
-    public void printList(MyLinkedList list) {
-        Node currNode = list.head;
-
-        if (currNode != null) {
-            System.out.print(currNode.data+" -> ");
-            currNode = currNode.next;
-        }
-    }
-
-    public MyLinkedList insertLast(MyLinkedList list, int data) {
-        Node newNode = new Node(data);
-
-        if (list.head == null) {
-            list.head = newNode;
-        } else {
-            Node last = list.head;
-            while (last.next != null) {
-                last = last.next;
-            }
-            last.next = newNode;
-        }
-        return list;
-    }
-
-    public MyLinkedList insertFirst(MyLinkedList list, int data) {
-        Node newNode = new Node(data);
-
-        if (list.head != null) {
-            newNode.next = list.head;
-        }
-        list.head = newNode;
-        return list;
     }
 }
